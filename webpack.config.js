455b70249c87b8fb61b6862d6deea6ff90896f95
VladislavYurin <vlad.yurin98@gmail.com>
2023-04-01 19:51:10 +0700

    cursor pointer in comms

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, "docs"),
 		filename: "main.js",
-		publicPath: "/Sber-dogfood/"
+		publicPath: "/Pixlpark_test/"
 	},
 	devServer: {
 		static: path.resolve(__dirname, "./docs"),
